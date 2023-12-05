17a01dcd760cdbc26695295304ded8756e801c64
SupermercadosNeves <149386828+SupermercadosNeves@users.noreply.github.com>
2023-12-05 16:08:44 -0300

    Upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function menu(){
+
+    let menu = document.querySelector('.mobile');
+    if (menu.classList.contains('open')){
+        menu.classList.remove('open');
+    } else{
+        menu.classList.add('open');
+    }
+}
